8410e378113ca774462252922374a0edb873d9c8
tolulope-kresha <akinbinutoluwalope@gmail.com>
2022-05-16 10:06:20 -0800

    some more changes

--- main.js
+++ main.js
@@ -3,10 +3,11 @@ const subMit = document.querySelector(".submit")
 const container = document.querySelector('.container')
 const container2 = document.querySelector('.container2')
 const number = document.getElementById('number')
+const activeBtn = document.querySelector('.active')
 
 btn.forEach(function(btns) {
     btns.addEventListener('click' , function (event) {
-      btns.classList.toggle('active')
+      activeBtn.classList.remove('active')
       let selectedBtn = event.currentTarget
      
       number.innerText = selectedBtn.innerText
